3881a6fc445f11727369a7d693095edd6f1f1a7f
= <froilan1512@gmail.com>
2017-08-24 05:28:05 -0500

    Generando reporte con JSPFD

---
+++ ReporteJSPDF/ReporteJSPDF/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReporteJSPDF.Models;
+using Newtonsoft.Json;
+
+namespace ReporteJSPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        ReporteEntities db = new ReporteEntities();
+
+        public ActionResult Order()
+        {
+            List<Orders> data = db.Orders.Take(5).OrderByDescending(x => x.OrderID).ToList();
+            ViewBag.List = new SelectList(data, "OrderID", "OrderID");
+            return View(data);
+        }
+
+        public dynamic _OrderDetails(int ID)
+        {
+            List<Order_Details> data = new List<Order_Details>();
+
+            db.Configuration.ProxyCreationEnabled = false;
+
+            data = db.Order_Details.Include("Products").Where(x => x.OrderID == ID).ToList();
+
+            return //Json(data);
+            Newtonsoft.Json.JsonConvert.SerializeObject(data,
+            Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ ReporteJSPDF/ReporteJSPDF/Models/OrderDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReporteJSPDF.Models
+{
+    public class OrderDetailsViewModel
+    {
+        public int OrderID { get; set; }
+        public string Producto { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+        public string Categoria { get; set; }
+    }
+}
